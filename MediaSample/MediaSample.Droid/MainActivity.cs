0bfde70021ccbb97c4f63f19f538dbcebf575ab9
Sandro Vecchiarelli <sandrov@live.it>
2017-09-14 00:33:42 +0200

    Add project files.

---
+++ MediaSample/MediaSample.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace MediaSample.Droid
+{
+    [Activity(Label = "MediaSample", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+			ZXing.Net.Mobile.Forms.Android.Platform.Init();
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+		{
+			global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+			
+    }
+}
+
---
+++ MediaSample/MediaSample/MediaPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace MediaSample
+{
+	public partial class MediaPage : ContentPage
+	{
+		public MediaPage()
+		{
+			InitializeComponent();
+
+			takePhoto.Clicked += async (sender, args) =>
+			{
+
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+					return;
+				}
+
+				var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
+					PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+					Directory = "Sample",
+					Name = "test.jpg"
+				});
+
+				if (file == null)
+					return;
+
+				await DisplayAlert("File Location", file.Path, "OK");
+
+				image.Source = ImageSource.FromStream(() =>
+		  {
+				  var stream = file.GetStream();
+				  file.Dispose();
+				  return stream;
+			  });
+			};
+
+			pickPhoto.Clicked += async (sender, args) =>
+			{
+				if (!CrossMedia.Current.IsPickPhotoSupported)
+				{
+					await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+					return;
+				}
+				var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+				{
+					PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+				});
+
+
+				if (file == null)
+					return;
+
+				image.Source = ImageSource.FromStream(() =>
+		  {
+				  var stream = file.GetStream();
+				  file.Dispose();
+				  return stream;
+			  });
+			};
+
+			takeVideo.Clicked += async (sender, args) =>
+			{
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+				{
+					await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+					return;
+				}
+
+				var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+				{
+					Name = "video.mp4",
+					Directory = "DefaultVideos",
+				});
+
+				if (file == null)
+					return;
+
+				await DisplayAlert("Video Recorded", "Location: " + file.Path, "OK");
+
+				file.Dispose();
+			};
+
+			pickVideo.Clicked += async (sender, args) =>
+			{
+				if (!CrossMedia.Current.IsPickVideoSupported)
+				{
+					await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+					return;
+				}
+				var file = await CrossMedia.Current.PickVideoAsync();
+
+				if (file == null)
+					return;
+
+				await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+				file.Dispose();
+			};
+
+		}
+
+		public void Btn_Qrcode_Click(object sender, EventArgs args)
+		{
+			var scanPage = new ZXingScannerPage();
+
+			scanPage.OnScanResult += (result) => {
+				// Stop scanning
+				scanPage.IsScanning = false;
+
+				// Pop the page and show the result
+				Device.BeginInvokeOnMainThread(() => {
+					Navigation.PopAsync();
+					DisplayAlert("Scanned Barcode", result.Text, "OK");
+				});
+			};
+			// Navigate to our scanner page
+			Navigation.PushAsync(scanPage);			
+		}
+	}
+}
